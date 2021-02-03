5c43f56e2d9d544c4f3bb85adde570525b364885
Zixin Zhang <1186193932@qq.com>
2021-02-03 10:51:04 +0800

    initialize repository

---
+++ MatlabTraining/QuadrupedLocomotionTraingTestMatlab/startTraining.m
@@ -0,0 +1,72 @@
+%% OPEN THE QUADRUPED ROBOT MODEL
+initializeRobotParameters;
+mdl = 'rlQuadrupedRobot';
+open_system(mdl);
+
+%% CREATE ENVIRONMENT INTERFACE
+% Specify the parameters for the observation set
+numObs = 44;
+obsInfo = rlNumericSpec([numObs 1]);
+obsInfo.Name = 'observations';
+
+% Specify the parameters for the action set
+numAct = 8;
+actInfo = rlNumericSpec([numAct 1],'LowerLimit',-1,'UpperLimit', 1);
+actInfo.Name = 'torque';
+
+% Create the environment using the reinforcement learning model
+blk = [mdl, '/RL Agent'];
+env = rlSimulinkEnv(mdl,blk,obsInfo,actInfo);
+
+% The reset function introduces random deviations into the initial joint angles and angular velocities during training
+env.ResetFcn = @quadrupedResetFcn;
+
+%% Create DDPG agent
+% Create the networks in the MATLAB workspace
+createNetworks;
+
+% View the critic network configuration
+plot(criticNetwork);
+
+% Specify the agent options using rlDDPGAgentOptions
+agentOptions = rlDDPGAgentOptions;
+agentOptions.SampleTime = Ts;
+agentOptions.DiscountFactor = 0.99;
+agentOptions.MiniBatchSize = 250;
+agentOptions.ExperienceBufferLength = 1e6;
+agentOptions.TargetSmoothFactor = 1e-3;
+agentOptions.NoiseOptions.MeanAttractionConstant = 0.15;
+agentOptions.NoiseOptions.Variance = 0.1;
+
+%Create the rlDDPGAgent object for the agent.
+agent = rlDDPGAgent(actor,critic,agentOptions);
+
+%% Specify Training Options
+maxEpisodes = 10000;
+maxSteps = floor(Tf/Ts);
+trainOpts = rlTrainingOptions(...
+    'MaxEpisodes',maxEpisodes,...
+    'MaxStepsPerEpisode',maxSteps,...
+    'ScoreAveragingWindowLength',250,...
+    'Verbose',true,...
+    'Plots','training-progress',...
+    'StopTrainingCriteria','AverageReward',...
+    'StopTrainingValue',190,...
+    'SaveAgentCriteria','EpisodeReward',...
+    'SaveAgentValue',200);
+
+% train the agent in parallel training mode
+trainOpts.UseParallel = true;
+trainOpts.ParallelizationOptions.Mode = 'async';
+trainOpts.ParallelizationOptions.StepsUntilDataIsSent = 32;
+trainOpts.ParallelizationOptions.DataToSendFromWorkers = 'Experiences';
+
+%% Train Agent
+doTraining = false;
+if doTraining    
+    % Train the agent
+    trainingStats = train(agent,env,trainOpts);
+else
+    % Load pretrained agent for the example
+    load('rlQuadrupedAgent.mat','agent')
+end
---
+++ UnityTraining/ANYmal/Library/PackageCache/com.unity.robotics.urdf-importer@68d91b3551/Runtime/UrdfComponents/UrdfRobot.cs
@@ -0,0 +1,136 @@
+﻿/*
+© Siemens AG, 2018
+Author: Suzannah Smith (suzannah.smith@siemens.com)
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+<http://www.apache.org/licenses/LICENSE-2.0>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace RosSharp.Urdf
+{
+    public enum GeometryTypes { Box, Cylinder, Sphere, Mesh }
+
+    public class UrdfRobot : MonoBehaviour
+    {
+        public string FilePath;
+        public ImportSettings.axisType choosenAxis ;
+        [SerializeField]
+        private ImportSettings.axisType currentOrientation = ImportSettings.axisType.yAxis;
+        public List<CollisionIgnore> collisionExceptions;
+        #region Configure Robot
+
+        public void SetCollidersConvex(bool convex)
+        {
+            foreach (MeshCollider meshCollider in GetComponentsInChildren<MeshCollider>())
+                meshCollider.convex = convex;
+        }
+
+
+        public void SetUseUrdfInertiaData(bool useUrdfData)
+        {
+            foreach (UrdfInertial urdfInertial in GetComponentsInChildren<UrdfInertial>())
+                urdfInertial.useUrdfData = useUrdfData;
+        }
+
+        public void SetRigidbodiesUseGravity(bool useGravity)
+        {
+#if UNITY_2020_1_OR_NEWER
+            foreach (ArticulationBody ar in GetComponentsInChildren<ArticulationBody>())
+                ar.useGravity = useGravity;
+#else
+            foreach (Rigidbody rb in GetComponentsInChildren<Rigidbody>())
+                rb.useGravity = useGravity;
+#endif
+        }
+
+        public void GenerateUniqueJointNames()
+        {
+            foreach (UrdfJoint urdfJoint in GetComponentsInChildren<UrdfJoint>())
+                urdfJoint.GenerateUniqueJointName();
+        }
+
+        // Add a rotation in the model which gives the correct correspondence between UnitySpace and RosSpace
+        public void ChangeToCorrectedSpace(bool rosSpace)
+        {
+            this.transform.Rotate(0, 180, 0);
+        }
+
+        public bool CheckOrientation()
+        {
+            return currentOrientation == choosenAxis;
+        }
+
+        public void SetOrientation()
+        {
+            currentOrientation = choosenAxis;
+        }
+
+        public void AddController(bool controller)
+        {
+            if (controller && this.gameObject.GetComponent< RosSharp.Control.Controller>() == null)
+            {
+                this.gameObject.AddComponent<RosSharp.Control.Controller>();
+            }
+            else
+            {
+                DestroyImmediate(this.gameObject.GetComponent<RosSharp.Control.Controller>());
+                DestroyImmediate(this.gameObject.GetComponent<RosSharp.Control.FKRobot>());
+                JointControl[] scriptList = GetComponentsInChildren<JointControl>();
+                foreach (JointControl script in scriptList)
+                    DestroyImmediate(script);
+            }
+        }
+
+        public void AddFkRobot(bool fkRobot)
+        {
+            if (fkRobot && this.gameObject.GetComponent<RosSharp.Control.FKRobot>() == null)
+            {
+                this.gameObject.AddComponent<RosSharp.Control.FKRobot>();
+            }
+            else
+            {
+                DestroyImmediate(this.gameObject.GetComponent<RosSharp.Control.FKRobot>());
+            }
+        }
+
+        public void SetAxis(ImportSettings.axisType setAxis)
+        {
+            this.choosenAxis = setAxis;
+        }
+
+        void Start()
+        {
+            CreateCollisionExceptions();
+        }
+
+        public void CreateCollisionExceptions()
+        {
+            if (collisionExceptions != null)
+            {
+                foreach (CollisionIgnore ignoreCollision in collisionExceptions)
+                {
+                    Collider[] collidersObject1 = ignoreCollision.Link1.GetComponentsInChildren<Collider>();
+                    Collider[] collidersObject2 = ignoreCollision.Link2.GetComponentsInChildren<Collider>();
+                    foreach (Collider colliderMesh1 in collidersObject1)
+                    {
+                        foreach (Collider colliderMesh2 in collidersObject2)
+                        {
+                            Physics.IgnoreCollision(colliderMesh1, colliderMesh2);
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
